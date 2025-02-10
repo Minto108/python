4edd9b1c8d6d85dc222269279ac104948e2825b7
Minto108 <158478797+Minto108@users.noreply.github.com>
2025-02-10 21:29:57 +0530

    Add files via upload

--- class.py
+++ class.py
@@ -1,10 +1,24 @@
 class Dog:
     species = "canine"
-
+    
     def __init__(self,name,age):
-        self.name = name
-        self.age = age
+        self._name = name
+        self.__age = age
+    def sound(self):
+        print("wooooof!!")
+    def get_age(self):
+        return self.__age
+
+class Labrador(Dog):
+    def sound(self):
+        print("Woof")
 
 dog1 = Dog("Harry",4)
-print(dog1)
-print(dog1.name)
+
+print(dog1._name)
+print(dog1.get_age())
+dog2 = Labrador("Ham",5)
+print(dog2._name)
+print(dog1._name)
+dog1.sound()
+dog2.sound()
